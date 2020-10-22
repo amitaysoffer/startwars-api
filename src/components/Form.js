00490dac12e4cc05691a14d11754f80f148996b4
amitaysoffer <amitaysof@gmail.com>
2020-10-22 14:18:40 +0100

    added images folder

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Form(props) {
   return (
     <div className="input-wrapper">
-      <input onChange={(e) => props.onSearchFilterChange(e)} 
-      type="text" 
-      id="search-filter" 
-      className="form-control" 
-      placeholder="May the force will be with whom?"></input>
+      <input onChange={(e) => props.onSearchFilterChange(e)}
+        type="text"
+        id="search-filter"
+        className="form-control"
+        placeholder="May the force will be with whom?"></input>
     </div>
   )
 }
