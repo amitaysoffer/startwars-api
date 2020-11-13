59ecacf624d463507082a2d925e8f8c683b20582
amitaysoffer <amitaysof@gmail.com>
2020-11-13 17:31:41 +0000

    updated states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Header from './components/Header'
 import Form from './components/Form'
@@ -8,10 +8,77 @@ import Pagination from './components/Pagination'
 import Spinner from './components/Spinner'
 
 function App() {
-  const [characters, setCharacters] = useState([])
-  const [filteredCharacters, setFilteredCharacters] = useState([])
-  const [spinner, setSpinner] = useState(true)
-  const [page, setPage] = useState(1)
+  const [characters, setCharacters] = useState([]);
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
+  const [spinner, setSpinner] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const [canSearch, setCanSearch] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+  const timeoutRef = useRef(null);
+  
+  function onSearchFilterChange(e) {
+    setCanSearch(false);
+    setSearchValue(e.target.value);
+  }
+
+  useEffect(() => {
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    if (canSearch) performApiCall();
+
+    timeoutRef.current = setTimeout(() => {
+      timeoutRef.current = null;
+      setCanSearch(true);
+    }, 1000);
+  }, [canSearch]);
+
+  async function getHomeAndSpeciesInfo(characters) {
+    for (const char of characters) {
+      const httpsRequestHomeWorld = `https${char.homeworld.substring(4)}`
+      const homeworldResponse = await axios.get(httpsRequestHomeWorld)
+      char.homeworld = homeworldResponse.data.name
+
+      if (char.species.length < 1) {
+        char.species = 'Human'
+      } else {
+        const httpsRequestSpecies = `https${char.species[0].substring(4)}`
+        const speciesdResponse = await axios.get(httpsRequestSpecies)
+        char.species = speciesdResponse.data.name
+      }
+    }
+    return characters
+  }
+
+  async function performApiCall() {
+    if (searchValue) {
+      const response = await axios.get(`https://swapi.dev/api/people/?search=${searchValue}`);
+      const characters = response.data.results;
+      const completeCharacters = await getHomeAndSpeciesInfo(characters);
+      setFilteredCharacters(completeCharacters)
+    } else {
+      setFilteredCharacters(characters)
+    }
+  }
+
+  async function displayCharacters() {
+    try {
+      const response = await axios.get(`https://swapi.dev/api/people/?page=${page}`);
+      const characters = response.data.results
+      const completeCharacters = await getHomeAndSpeciesInfo(characters);
+      setCharacters(completeCharacters)
+      setFilteredCharacters(completeCharacters)
+      setSpinner(prevSpinner => prevSpinner = false)
+    } catch (err) {
+      console.log('error in promise, ', err)
+    }
+  }
+
+  useEffect(() => {
+    displayCharacters();
+  }, [page])
 
   function handleNumberClick(event) {
     setPage(parseInt(event.target.text))
@@ -29,64 +96,11 @@ function App() {
     }
   }
 
-  async function onSearchFilterChange(e) {
-    if (e.target.value) {
-      const response = await axios.get(`https://swapi.dev/api/people/?search=${e.target.value}`);
-      const characters = response.data.results;
-
-      for (const char of characters) {
-        const httpsRequestHomeWorld = `https${char.homeworld.substring(4)}`
-        const homeworldResponse = await axios.get(httpsRequestHomeWorld)
-        char.homeworld = homeworldResponse.data.name
-
-        if (char.species.length < 1) {
-          char.species = 'Human'
-        } else {
-          const httpsRequestSpecies = `https${char.species[0].substring(4)}`
-          const speciesdResponse = await axios.get(httpsRequestSpecies)
-          char.species = speciesdResponse.data.name
-        }
-      }
-      setFilteredCharacters(prevFilteredCharacters => prevFilteredCharacters = characters)
-    } else {
-      setFilteredCharacters(prevFilteredCharacters => prevFilteredCharacters = characters)
-    }
-  }
-
-  useEffect(() => {
-    async function displayCharacters() {
-      try {
-        const response = await axios.get(`https://swapi.dev/api/people/?page=${page}`);
-        const characters = response.data.results
-
-        for (const char of characters) {
-          const httpsRequestHomeWorld = `https${char.homeworld.substring(4)}`
-          const homeworldResponse = await axios.get(httpsRequestHomeWorld)
-          char.homeworld = homeworldResponse.data.name
-
-          if (char.species.length < 1) {
-            char.species = 'Human'
-          } else {
-            const httpsRequestSpecies = `https${char.species[0].substring(4)}`
-            const speciesdResponse = await axios.get(httpsRequestSpecies)
-            char.species = speciesdResponse.data.name
-          }
-        }
-        setCharacters(prevCharacters => prevCharacters = characters)
-        setFilteredCharacters(prevFilteredCharacters => prevFilteredCharacters = characters)
-        setSpinner(prevSpinner => prevSpinner = false)
-      } catch (err) {
-        console.log('error in promise, ', err)
-      }
-    }
-    displayCharacters();
-  }, [page])
-
   return (
     <div className="app container">
       <Header />
-      <Form characters={characters} onSearchFilterChange={onSearchFilterChange} />
-      {spinner ? <Spinner /> : <DisplayTable characters={characters} filteredCharacters={filteredCharacters} />}
+      <Form characters={filteredCharacters} onSearchFilterChange={onSearchFilterChange} />
+      {spinner ? <Spinner /> : <DisplayTable filteredCharacters={filteredCharacters} />}
       <Pagination
         page={page}
         handleNumberClick={handleNumberClick}
